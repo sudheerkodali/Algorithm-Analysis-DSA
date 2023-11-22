e5a8a902428e3a2ee245611a7bd4abd3b2e90621
sudheer chowdari kodali <45354847+sudheerkodali@users.noreply.github.com>
2023-11-22 14:01:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+#Algorithm analysis on Time complexity and Space complexity
+  
+/ Time complexity example: Linear search
 
+function linearSearch(array, target) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === target) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+
+
+// Time complexity analysis: O(n)
+
+const array = [1, 2, 3, 4, 5];
+const target = 4;
+const index = linearSearch(array, target);
+
+if (index !== -1) {
+  console.log('Element found at index:', index);
+} else {
+  console.log('Element not found');
+}
